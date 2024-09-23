ef07b5292bdc3e49c8740e81d1271a4021835b4b
TrinhThuong2001 <88668726+JasmineVan@users.noreply.github.com>
2024-09-23 22:38:57 +0700

    fix: deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -15,5 +14,3 @@ root.render(
     <App/>
   </React.StrictMode>,
 );
-
-reportWebVitals();
